31aeecc1d177f10ade0883328fc0e414905b1aa2
Adrian Wysocki <dobrystuff77@gmail.com>
2020-05-01 23:11:45 +0200

    configuration server.js for deploying

--- server.js
+++ server.js
@@ -71,6 +71,15 @@ app.delete("/todo/:id", (req, res) => {
     });
 });
 
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(process.env.PORT || port, () =>
   console.log(`Server running on port ${port}`)
 );
